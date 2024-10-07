bf4dd8159ff310855c93a1269f69406278aec0dd
hufeng <1287973118@qq.com>
2024-10-07 23:00:25 +0800

    Site updated: 2024-10-07 23:00:25

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1728312618891";
+const VERSION = "1728313221160";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
