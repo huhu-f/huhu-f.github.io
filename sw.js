e2c781c94b6d5fef222aa081e65bfc3e9f982c32
zhoucheng <2058856398@qq.com>
2024-10-07 22:43:16 +0800

    Site updated: 2024-10-07 22:43:16

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1728310859716";
+const VERSION = "1728312196344";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
